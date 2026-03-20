9627028591082da4c2baef8064042feb1edee755
Olowokere_Destiny <destinyolowokere@gmail.com>
2026-03-20 12:52:17 +0100

    join steps

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,10 +1,12 @@
 import JoinHero from '@/components/JoinHero'
+import JoinSteps from '@/components/JoinSteps'
 import React from 'react'
 
 const page = () => {
   return (
     <>
     <JoinHero />
+    <JoinSteps />
     </>
   )
 }
