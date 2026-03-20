6067d5a70f3508e40328e293f38e75deeb3db025
Olowokere_Destiny <destinyolowokere@gmail.com>
2026-03-20 15:55:06 +0100

    updates

--- components/JoinHero.tsx
+++ components/JoinHero.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import heroBg from "../assets/images/join-hero-bg.png";
 import googlePlayBadge from "../assets/images/google-play-badge.png";
+
 export default function JoinHero() {
   return (
-    <section className="relative w-full h-190 md:h-150 lg:landscape:h-[90vh] xl:landscape:h-[90vh] xl:landscape:pt-4 overflow-hidden sm:pb-4">
+    <section className="relative w-full h-190 md:h-150 lg:landscape:h-screen xl:landscape:h-[90vh]  lg:landscape:pt-4 overflow-hidden sm:pb-4">
       <div className="absolute -top-16 -left-16 w-52 md:w-72 h-52 md:h-72 rounded-full bg-[#FF3E1D]/10 blur-2xl pointer-events-none z-0" />
 
       <div className="absolute pointer-events-none z-0 bottom-0 right-0 w-full h-56 sm:w-3/4 sm:h-72 md:w-[55%] md:h-full">
@@ -16,12 +17,13 @@ export default function JoinHero() {
         />
       </div>
 
-      <div className="relative z-10 -translate-y-20 sm:translate-y-0 xl:landscape:-translate-y-20 flex flex-col justify-center h-full max-w-7xl mx-auto px-6 md:px-12">
+      <div className="relative z-10 -translate-y-20 sm:translate-y-0 lg:landscape:-translate-y-4 xl:-translate-y-8 flex flex-col justify-center h-full max-w-7xl mx-auto px-6 md:px-12">
         <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-[45px] font-semibold leading-tight mb-6">
           <span className="text-[#FF3E1D]">Drive, Rent,</span> and{" "}
           <span className="text-[#FF3E1D]">Grow</span> with AAVORide Partner
         </h1>
-        <div className="sm:max-w-sm md:max-w-[70%] lg:max-w-[60%] flex flex-col gap-6">
+        
+        <div className="sm:max-w-sm md:max-w-[70%] flex flex-col gap-6">
           <div className="text-sm leading-relaxed lg:text-[14px] xl:text-xl flex flex-col gap-2">
             <p>
               AAVORide Partner is a smart mobility platform designed for
@@ -42,23 +44,22 @@ export default function JoinHero() {
               partners and riders.
             </p>
           </div>
-        </div>
-        <div className="bg-[#76767699]/60 rounded-xs px-4 py-3 flex flex-col gap-3 w-full md:w-fit">
-          <div className="flex-1 min-w-0">
-            <p className="text-xs sm:text-sm font-semibold leading-tight">
+
+          <div className="bg-[#76767699]/60 rounded-md shadow-xs px-4 py-3 flex flex-col gap-2 sm:gap-6 w-full sm:w-fit lg:landscape:translate-y-4">
+            <p className="text-sm lg:text-lg font-semibold leading-tight">
               Download AAVORide Partner
             </p>
-            <p className="text-xs mt-2 leading-tight">
+            <p className="text-sm">
               Create Your Partner Account and Start Earning Today
             </p>
+            <Image
+              src={googlePlayBadge}
+              alt="Get it on Google Play"
+              width={90}
+              height={28}
+              className="shrink-0 w-20 sm:w-24 self-end mt-1"
+            />
           </div>
-          <Image
-            src={googlePlayBadge}
-            alt="Get it on Google Play"
-            width={90}
-            height={28}
-            className="shrink-0 w-20 sm:w-22.5 self-end"
-          />
         </div>
       </div>
     </section>
